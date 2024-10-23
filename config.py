0f580c8951bbad55b67f8a90afcdb43f7548ea3b
Keshihan <keshihan007@gmail.com>
2024-10-23 11:45:42 +0530

    keshihan weights

---
+++ config.py
@@ -0,0 +1,12 @@
+EPOCHS = 5
+BETA_1 = 0.5
+BATCH_SIZE = 4
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+HSI_CHANNELS = 31
+LEARNING_RATE = 2e-4
+CHECKPOINT_DIR = '.\\checkpoints'
+CHECKPOINT_PREFIX = "keshihan_ckpt"
+LOG_DIR = ".\\logs"
+RGB_IMAGE_PATH = ".\\data\\rgb"
+HSI_IMAGE_PATH = ".\\data\\post"
