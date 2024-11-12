5a390696453f5805a46d20bfcf96bdceedae5661
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2024-11-12 15:47:22 +0530

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,19 +1,30 @@
 import tensorflow as tf
+from model import PerceptualLoss
+
+def gradient_penalty(real, fake, discriminator):
+    alpha = tf.random.uniform(shape=[real.shape[0], 1, 1, 1], minval=0., maxval=1.)
+    interpolated = alpha * real + (1 - alpha) * fake
+    with tf.GradientTape() as tape:
+        tape.watch(interpolated)
+        pred = discriminator(interpolated)
+    grads = tape.gradient(pred, [interpolated])[0]
+    norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=[1, 2, 3]))
+    gp = tf.reduce_mean((norm - 1.0) ** 2)
+    return gp
+
+def discriminator_loss(real_output, fake_output, real, fake, discriminator):
+    real_loss = tf.keras.losses.binary_crossentropy(tf.ones_like(real_output), real_output)
+    fake_loss = tf.keras.losses.binary_crossentropy(tf.zeros_like(fake_output), fake_output)
+    gp = gradient_penalty(real, fake, discriminator)
+    return tf.reduce_mean(real_loss + fake_loss) + 10.0 * gp  # Add gradient penalty
 
 
-def discriminator_loss(real_output, fake_output):
-    real_loss = tf.keras.losses.binary_crossentropy(
-        tf.ones_like(real_output), real_output)
-    fake_loss = tf.keras.losses.binary_crossentropy(
-        tf.zeros_like(fake_output), fake_output)
-    return tf.reduce_mean(real_loss + fake_loss)
-
-
-def generator_loss(fake_output, generated, target, lambda_pixel=10):
+def generator_loss(fake_output, generated, target, lambda_pixel=10, lambda_perceptual=0.1):
     adv_loss = tf.reduce_mean(tf.keras.losses.binary_crossentropy(
         tf.ones_like(fake_output), fake_output))
     pixel_loss_value = pixel_loss(generated, target)
-    return adv_loss + lambda_pixel * pixel_loss_value
+    perceptual_loss_value = PerceptualLoss()(target, generated)  # PerceptualLoss is used here
+    return adv_loss + lambda_pixel * pixel_loss_value + lambda_perceptual * perceptual_loss_value
 
 
 def pixel_loss(generated, target):
