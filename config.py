0e7567e6cbf1f75ce804bf978e0e8612f6fbf266
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2024-11-19 15:13:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-EPOCHS = 20
+EPOCHS = 1
 BETA_1 = 0.5
 BATCH_SIZE = 4
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
 HSI_CHANNELS = 31
 LEARNING_RATE = 1e-5
-CHECKPOINT_DIR = 'checkpoints/'
+CHECKPOINT_DIR = r'C:\Harshi\ecs-venv\rgb-to-hyper\rgb-to-hyper-main\rgb-to-hyper\checkpoints'
 GLOBAL_CHECKPOINT_PREFIX = "global_ckpt"
-LOCAL_CHECKPOINT_PREFIX = "akshath_ckpt"
+LOCAL_CHECKPOINT_PREFIX = "harshitha_ckpt"
 LOG_DIR = "logs/"
 MAT_IMAGE_PATH = r"c:\Harshi\ECS-II\Dataset\HSI_mat"
 RGB_IMAGE_PATH = r"c:\Harshi\ECS-II\Dataset\RGB_my_chunk"
